a700e89155ceba4efc9e00eeaa45648cdd9d3490
Ashi Krishnan <me@ashi.works>
2018-03-15 16:03:01 -0400

    Initial firebase setup

--- fire/setup.js
+++ fire/setup.js
@@ -1,1 +1,14 @@
-throw new Error('fire/setup.js does not contain a config. You need to run `npm prepare`')
+module.exports = firebase =>
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  "apiKey": "AIzaSyDCowB_mdhwb8kMhbV_HMmwD_0NWcbhfx0",
+  "databaseURL": "https://my-awesome-project-60479.firebaseio.com",
+  "storageBucket": "my-awesome-project-60479.appspot.com",
+  "authDomain": "my-awesome-project-60479.firebaseapp.com",
+  "messagingSenderId": "870144413679",
+  "projectId": "my-awesome-project-60479"
+});
+
